9abcc3b5038da6cc559fa02eb43a0718a064b0a0
User <sobirjonovoyatiilo37@gmail.com>
2023-02-21 10:35:38 +0500

    oiiyio

---
+++ dars4.py
@@ -0,0 +1,7 @@
+ball = int(input("ballni kiriting"))
+eilts = float(input("eiltsni kiriting"))
+
+if ball > 100 or  eilts >=5.5:
+    print("imtihondan otti")
+else:
+    print("imtihondan yiqildi")
